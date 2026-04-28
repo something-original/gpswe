3df70a4036433fe997808cbf021264ee472f1bfb
Georgiy Kiselev <gsx2002@yandex.ru>
2026-04-28 14:03:57 +0300

    settings corrected

--- gpswe/config/settings.py
+++ gpswe/config/settings.py
@@ -7,29 +7,15 @@ class ServerSettings(BaseModel):
     Пользовательские настройки библиотеки
     """
 
-    BUFF_SIZE: Optional[int]
-    HOST: Optional[str]
-    PORT: Optional[int]
-    WIALON_PROTOCOL_VERSION: Optional[str]
-    EGTS_PROTOCOL_VERSION: Optional[str]
-    POSTGRES_DB: Optional[str]
-    POSTGRES_USER: Optional[str]
-    POSTGRES_PASSWORD: Optional[str]
-    POSTGRES_HOST: Optional[str]
-    POSTGRES_PORT: Optional[str]
+    BUFF_SIZE: Optional[int] = 8192
+    HOST: Optional[str] = "0.0.0.0"
+    PORT: Optional[int] = 10500
+    WIALON_PROTOCOL_VERSION: Optional[str] = "2.0"
+    EGTS_PROTOCOL_VERSION: Optional[str] = "1.0"
+    POSTGRES_DB: Optional[str] = "gpswe_test"
+    POSTGRES_USER: Optional[str] = "test"
+    POSTGRES_PASSWORD: Optional[str] = "pass"
+    POSTGRES_HOST: Optional[str] = "0.0.0.0"
+    POSTGRES_PORT: Optional[str] = "5432"
 
 settings = ServerSettings()
-
-"""
-Инициализация настроек для тестирования
-"""
-
-settings.BUFF_SIZE = 8192
-settings.HOST = "0.0.0.0"
-settings.PORT = 10500
-settings.WIALON_PROTOCOL_VERSION = "2.0"
-settings.POSTGRES_DB = "gpswe_test"
-settings.POSTGRES_USER = "test"
-settings.POSTGRES_PASSWORD = "pass"
-settings.POSTGRES_HOST = "0.0.0.0"
-settings.POSTGRES_PORT = "5432"
